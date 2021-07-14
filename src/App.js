7856eaa41a76d8a4a8f03b8d19edc36e493dba9b
TatianaNarizhna <narizhna.tania@gmail.com>
2021-07-14 22:13:11 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Container from "./components/Container/Container";
+import Input from "./components/ContactForm/InputForm";
 import PhonebookList from "./components/PhonebookList/PhonebookList";
 
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    name: "",
+  };
+
+  formSubmit = (data) => {
+    console.log(data);
+    const contactData = {
+      id: uuidv4(),
+      name: data.name,
+      number: data.number,
+    };
+    this.setState((prevState) => ({
+      contacts: [contactData, ...prevState.contacts],
+    }));
+  };
+
+  deleteList = (listId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== listId),
+    }));
   };
 
   render() {
-    const { contacts, name } = this.state;
-    return <PhonebookList contacts={contacts} title="Contacts" />;
+    const { contacts } = this.state;
+    return (
+      <Container>
+        <Input onFormSubmit={this.formSubmit} />
+        <PhonebookList
+          contacts={contacts}
+          title="Contacts"
+          onDeleteList={this.deleteList}
+        />
+      </Container>
+    );
   }
 }
-
-// const App = () => {
-//   return <Phonebook />
-
-// }
 
 export default App;
--- src/components/PhonebookList/PhonebookList.js
+++ src/components/PhonebookList/PhonebookList.js
@@ -2,16 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import s from "./PhonebookList.module.css";
 
-const PhonebookList = ({ contacts, title }) => {
+const PhonebookList = ({ contacts, title, onDeleteList }) => {
   return (
-    <div className={s.listContainer}>
+    <div>
       <h2>{title}</h2>
       <ul>
         {contacts.map(({ id, name, number }) => (
           <li className={s.list} key={id}>
             <p>{name}</p>
             <p>{number}</p>
-            <button>Delete</button>
+            <button onClick={() => onDeleteList(id)}>Delete</button>
           </li>
         ))}
       </ul>
